1073f2f8ec069a8c7ecaa107470e2114a7b6cb9d
Mike Cantrell <mike@devnull.org>
2012-07-20 18:08:03 -0500

    updated registration handler to add default roles upon authentication

---
+++ zuul-data/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+insert into Application (id, key, name) values (1, 'FOO','Test Application')
+
+insert into SecurityRole (id, name) values (1, 'ROLE_GUEST')
+insert into SecurityRole (id, name) values (2, 'ROLE_USER')
+insert into SecurityRole (id, name) values (3, 'ROLE_ADMIN')
+insert into SecurityRole (id, name) values (4, 'ROLE_SYSTEM_ADMIN')
+
+-- insert into SecurityUser (id, openId, firstName, lastName, email, enabled) values (1, 'https://www.google.com/accounts/o8/id?id=AItOawnlnuHfoKGwMJSjRHxBROwqil0OE84Zscc', 'Mike', 'Cantrell', 'mike@google.com', true)
+insert into SecurityUser (id, openId, firstName, lastName, email, enabled) values (2, 'https://me.yahoo.com/a/mMz2C510uMjhwvHr.4K2aToLWzrPDJb.._M-#b431e', 'Mike', 'Cantrell', 'mike@yahoo.com', true)
+
+insert into SecurityUserRole(userId, roleId) values (2, 4)
--- zuul-data/src/test/resources/import.sql
+++
@@ -1,8 +0,0 @@
-insert into Application (id, key, name) values (1, 'FOO','Test Application')
-
-insert into SecurityRole (id, name) values (1, 'ROLE_USER')
-insert into SecurityRole (id, name) values (2, 'ROLE_ADMIN')
-insert into SecurityRole (id, name) values (3, 'ROLE_SYSTEM_ADMIN')
-
-insert into SecurityUser (id, openId, firstName, lastName, email, enabled) values (1, 'https://www.google.com/accounts/o8/id?id=AItOawnlnuHfoKGwMJSjRHxBROwqil0OE84Zscc', 'Mike', 'Cantrell', 'mike@google.com', true)
-insert into SecurityUser (id, openId, firstName, lastName, email, enabled) values (2, 'https://me.yahoo.com/a/mMz2C510uMjhwvHr.4K2aToLWzrPDJb.._M-#b431e', 'Mike', 'Cantrell', 'mike@yahoo.com', true)
