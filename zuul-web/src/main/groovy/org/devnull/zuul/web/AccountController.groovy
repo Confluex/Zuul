2edeafce09240c029bee38fb98dca046c0614033
Mike Cantrell <mike@devnull.org>
2012-07-22 09:41:29 -0500

    replaced call to securityService.register with add/remove roles

--- zuul-web/src/main/groovy/org/devnull/zuul/web/AccountController.groovy
+++ zuul-web/src/main/groovy/org/devnull/zuul/web/AccountController.groovy
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.devnull.zuul.web.config.WebConstants
 
 @Controller
 class AccountController {
@@ -31,8 +32,9 @@ class AccountController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     String registerSubmit(@ModelAttribute User user) {
-        securityService.save(user)
-        securityService.register(user.id)
+        securityService.updateCurrentUser(user)
+        securityService.addRoles([WebConstants.ROLE_USER])
+        securityService.removeRoles([WebConstants.ROLE_GUEST])
         return "redirect:/welcome"
     }
 
---
+++ zuul-web/src/main/groovy/org/devnull/zuul/web/config/WebConstants.groovy
@@ -0,0 +1,8 @@
+package org.devnull.zuul.web.config
+
+class WebConstants {
+    static final String ROLE_GUEST = "ROLE_GUEST"
+    static final String ROLE_USER = "ROLE_USER"
+    static final String ROLE_ADMIN = "ROLE_ADMIN"
+    static final String ROLE_SYSTEM_ADMIN = "ROLE_SYSTEM_ADMIN"
+}
