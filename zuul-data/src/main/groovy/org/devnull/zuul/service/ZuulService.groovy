d19dd49f6c4ee087732af2d5369dff8ad07a8e41
Mike Cantrell <mike@devnull.org>
2012-08-14 19:29:38 -0500

    implemented role hierarchy and secured admin methods on ZuulService (ui still needs lock down)

--- zuul-data/src/main/groovy/org/devnull/zuul/service/ZuulService.groovy
+++ zuul-data/src/main/groovy/org/devnull/zuul/service/ZuulService.groovy
@@ -1,33 +1,57 @@
 package org.devnull.zuul.service
 
-import org.devnull.zuul.data.model.SettingsGroup
-import com.google.inject.internal.Strings
 import org.devnull.zuul.data.model.Environment
 import org.devnull.zuul.data.model.SettingsEntry
-import org.devnull.zuul.data.model.EncryptionKey
+import org.devnull.zuul.data.model.SettingsGroup
+import org.springframework.security.access.prepost.PreAuthorize
 
 public interface ZuulService {
 
     /* Settings Groups -------------------- */
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     SettingsGroup createEmptySettingsGroup(String groupName, String environmentName)
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     SettingsGroup createSettingsGroupFromPropertiesFile(String name, String env, InputStream inputStream)
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     SettingsGroup createSettingsGroupFromCopy(String name, String env, SettingsGroup copy)
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void deleteSettingsGroup(Integer groupId)
+
+
     List<SettingsGroup> findSettingsGroupByName(String name)
+
+
     SettingsGroup findSettingsGroupByNameAndEnvironment(String name, String env)
+
+
     List<SettingsGroup> listSettingsGroups()
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     SettingsGroup save(SettingsGroup group)
 
     /* Environments -------------------- */
+
     List<Environment> listEnvironments()
 
     /* Settings Entry -------------------- */
-    SettingsEntry encryptSettingsEntryValue(Integer entryId)
-    SettingsEntry decryptSettingsEntryValue(Integer entryId)
-    SettingsEntry findSettingsEntry(Integer id)
-    void deleteSettingsEntry(Integer entryId)
-    SettingsEntry save(SettingsEntry entry)
 
+    SettingsEntry findSettingsEntry(Integer id)
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    SettingsEntry encryptSettingsEntryValue(Integer entryId)
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    SettingsEntry decryptSettingsEntryValue(Integer entryId)
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void deleteSettingsEntry(Integer entryId)
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    SettingsEntry save(SettingsEntry entry)
 
 
 }
