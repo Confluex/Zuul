d79c23742f930ea5b96eb101dd4a510fa258b588
Mike Cantrell <mike@devnull.org>
2012-08-16 19:08:51 -0500

    removed unused imports

--- zuul-web/src/main/groovy/org/devnull/zuul/web/SystemAdminServicesController.groovy
+++ zuul-web/src/main/groovy/org/devnull/zuul/web/SystemAdminServicesController.groovy
@@ -4,12 +4,10 @@ import org.devnull.security.service.SecurityService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.servlet.ModelAndView
-import sun.plugin.liveconnect.ReplaceMethod
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.PathVariable
-import javax.servlet.http.HttpServletResponse
 import org.springframework.web.bind.annotation.RequestParam
+
+import javax.servlet.http.HttpServletResponse
 
 @Controller
 class SystemAdminServicesController {
@@ -17,7 +15,7 @@ class SystemAdminServicesController {
     @Autowired
     SecurityService securityService
 
-    @RequestMapping(value="/admin/system/user/role", method=RequestMethod.DELETE)
+    @RequestMapping(value = "/admin/system/user/role", method = RequestMethod.DELETE)
     void removeRoleFromUser(HttpServletResponse response, @RequestParam("roleId") Integer roleId, @RequestParam("userId") Integer userId) {
         securityService.removeRoleFromUser(roleId, userId)
         response.status = HttpServletResponse.SC_NO_CONTENT
