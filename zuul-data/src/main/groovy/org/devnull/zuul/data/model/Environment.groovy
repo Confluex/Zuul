abad87bc5da565b048e4cbbb36e92a2ccf2926ce
Mike Cantrell <mike@devnull.org>
2012-09-16 10:08:03 -0500

    restricted environment names to numbers and letters

--- zuul-data/src/main/groovy/org/devnull/zuul/data/model/Environment.groovy
+++ zuul-data/src/main/groovy/org/devnull/zuul/data/model/Environment.groovy
@@ -10,6 +10,9 @@ import javax.persistence.CascadeType
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.OneToMany
+import javax.validation.constraints.Pattern
+import javax.persistence.Column
+import javax.validation.constraints.Size
 
 @Entity
 @EqualsAndHashCode(includes = "name")
@@ -20,6 +23,9 @@ class Environment implements Serializable {
     static final long serialVersionUID = ZuulDataConstants.API_VERSION
 
     @Id
+    @Pattern(regexp="^[a-zA-Z0-9]+\$/", message="Name can only contain letters and numbers")
+    @Size(min=1, max=12, message="Name must be 1-12 characters long")
+    @Column(length=12)
     String name
 
 
