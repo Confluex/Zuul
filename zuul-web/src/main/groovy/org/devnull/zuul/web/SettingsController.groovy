f46727fe0ce87cbef716fda5b04ee6acbb65ae25
Mike Cantrell <mike@devnull.org>
2012-11-13 19:55:02 -0600

    removed grid editor work (for now)

--- zuul-web/src/main/groovy/org/devnull/zuul/web/SettingsController.groovy
+++ zuul-web/src/main/groovy/org/devnull/zuul/web/SettingsController.groovy
@@ -7,12 +7,15 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
 
 import javax.servlet.http.HttpServletRequest
 import javax.validation.Valid
-
-import org.springframework.web.bind.annotation.*
 
 @Controller
 class SettingsController {
@@ -108,12 +111,6 @@ class SettingsController {
         return new ModelAndView("/settings/show", model)
     }
 
-    @RequestMapping(value = "/settings/{environment}/{name}", method = RequestMethod.GET)
-    ModelAndView showGroupByEnv(@PathVariable("name") String groupName, @PathVariable("environment") envName) {
-        def group = zuulService.findSettingsGroupByNameAndEnvironment(groupName, envName)
-        def model = [group: group]
-        return new ModelAndView("/settings/test", model)
-    }
 
 
     @RequestMapping(value = "/settings/{environment}/{groupName}/key/change", method = RequestMethod.GET)
