0fe33d8aeb005b108dd8289d9b651b87485457d1
Mike Cantrell <mike@devnull.org>
2012-11-16 11:29:25 -0600

    added unit test for sort criteria on audit controller

--- zuul-web/src/test/groovy/org/devnull/zuul/web/AuditControllerTest.groovy
+++ zuul-web/src/test/groovy/org/devnull/zuul/web/AuditControllerTest.groovy
@@ -1,10 +1,13 @@
 package org.devnull.zuul.web
 
+import org.devnull.util.pagination.HttpRequestPagination
 import org.devnull.util.pagination.Pagination
+import org.devnull.util.pagination.Sort
 import org.devnull.zuul.data.model.SettingsAudit
 import org.devnull.zuul.service.ZuulService
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentCaptor
 import org.mockito.Matchers
 import org.springframework.mock.web.MockHttpServletRequest
 
@@ -24,6 +27,20 @@ class AuditControllerTest {
         when(controller.zuulService.findSettingAudits(Matchers.any(Pagination))).thenReturn(audits)
         def results = controller.findAudits(new MockHttpServletRequest())
         assert audits == results
+    }
+
+    @Test
+    void shouldApplySortCriteriaWhenFindingAudits() {
+        def request = new MockHttpServletRequest()
+        request.setParameter("sort", "modifiedBy")
+        request.setParameter("dir", "DESC")
+        controller.findAudits(request)
+        def args = ArgumentCaptor.forClass(HttpRequestPagination)
+        verify(controller.zuulService).findSettingAudits(args.capture())
+        assert args.value.sorts.size() == 1
+        def sort = args.value.sorts.first()
+        assert sort.direction == Sort.DESC
+        assert sort.field == "modifiedBy"
     }
 
     @Test
