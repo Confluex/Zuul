0a8f13c8eaddfbec13760c426f1605c3b12d1f8e
Mike Cantrell <mike@devnull.org>
2012-12-30 10:00:37 -0600

    moved zuul-spring-client to top level project: https://github.com/mcantrell/zuul-spring-client

--- zuul-spring-client/src/main/groovy/org/devnull/client/spring/cache/PropertiesObjectStore.groovy
+++
@@ -1,6 +0,0 @@
-package org.devnull.client.spring.cache
-
-public interface PropertiesObjectStore {
-    void put(String environment, String name, Properties properties)
-    Properties get(String environment, String name)
-}
--- zuul-spring-client/src/main/groovy/org/devnull/client/spring/namespace/ZuulSpringClientNamespaceHandler.groovy
+++
@@ -1,9 +0,0 @@
-package org.devnull.client.spring.namespace
-
-import org.springframework.beans.factory.xml.NamespaceHandlerSupport
-
-class ZuulSpringClientNamespaceHandler extends NamespaceHandlerSupport  {
-    void init() {
-        registerBeanDefinitionParser("properties", new ZuulPropertiesBeanDefinitionParser());
-    }
-}
