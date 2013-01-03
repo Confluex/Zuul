27363e68f979d54ab030c5e2b1a07e50f6fe463f
Mike Cantrell <mike@devnull.org>
2013-01-02 22:15:01 -0600

    added integration for each configured encryption algorithm

--- zuul-data/src/test/groovy/org/devnull/zuul/service/security/EncryptionStrategyIntegrationTest.groovy
+++ zuul-data/src/test/groovy/org/devnull/zuul/service/security/EncryptionStrategyIntegrationTest.groovy
@@ -6,19 +6,27 @@ import org.devnull.zuul.data.test.ZuulDataIntegrationTest
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 
+import javax.annotation.Resource
+
 @Slf4j
 class EncryptionStrategyIntegrationTest extends ZuulDataIntegrationTest {
     @Autowired
     EncryptionStrategy strategy
 
+    @Resource
+    List<KeyConfiguration> keyConfigurations
+
     @Test
     void shouldEncryptAndDecryptTextByKey() {
-        def key = new EncryptionKey(password: "test")
-        def encrypted = strategy.encrypt("abc", key)
-        assert encrypted != "abc"
-        log.info("Encrypted: {}", encrypted)
-        def decrypted = strategy.decrypt(encrypted, key)
-        assert decrypted == "abc"
+        assert keyConfigurations
+        keyConfigurations.each { config ->
+            def key = new EncryptionKey(password: "test", algorithm: config.algorithm)
+            def encrypted = strategy.encrypt("abc", key)
+            assert encrypted != "abc"
+            log.info("{}: Encrypted: {}", key.algorithm, encrypted)
+            def decrypted = strategy.decrypt(encrypted, key)
+            assert decrypted == "abc"
+        }
     }
 
 
