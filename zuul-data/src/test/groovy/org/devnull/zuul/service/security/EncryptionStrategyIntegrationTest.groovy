29b4482b1d3f3bd39f6066975cd316c52ad019c3
Mike Cantrell <mike@devnull.org>
2013-01-03 16:38:27 -0600

    removed catch put in for travis ci missing policy files

--- zuul-data/src/test/groovy/org/devnull/zuul/service/security/EncryptionStrategyIntegrationTest.groovy
+++ zuul-data/src/test/groovy/org/devnull/zuul/service/security/EncryptionStrategyIntegrationTest.groovy
@@ -3,7 +3,6 @@ package org.devnull.zuul.service.security
 import groovy.util.logging.Slf4j
 import org.devnull.zuul.data.model.EncryptionKey
 import org.devnull.zuul.data.test.ZuulDataIntegrationTest
-import org.jasypt.exceptions.EncryptionOperationNotPossibleException
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -14,30 +13,19 @@ class EncryptionStrategyIntegrationTest extends ZuulDataIntegrationTest {
     @Autowired
     EncryptionStrategy strategy
 
-    @Resource(name='keyMetaData')
+    @Resource(name = 'keyMetaData')
     Map<String, KeyConfiguration> keyMetData
 
     @Test
     void shouldEncryptAndDecryptTextByKey() {
         assert keyMetData
         keyMetData.each { name, config ->
-            try {
-                def key = new EncryptionKey(password: "test", algorithm: config.algorithm)
-                def encrypted = strategy.encrypt("abc", key)
-                assert encrypted != "abc"
-                log.info("{}: Encrypted: {}", key.algorithm, encrypted)
-                def decrypted = strategy.decrypt(encrypted, key)
-                assert decrypted == "abc"
-            } catch (EncryptionOperationNotPossibleException e) {
-                // I hate doing this but Travis CI does not have these installed
-                // Opened request to install policy files: https://github.com/travis-ci/travis-ci/issues/832
-                if (e.message.contains("Unlimited Strength Jurisdiction Policy Files")) {
-                    log.error("Ignoring test failure because JCE policy files might not be installed", e)
-                }
-                else {
-                    throw(e)
-                }
-            }
+            def key = new EncryptionKey(password: "test", algorithm: config.algorithm)
+            def encrypted = strategy.encrypt("abc", key)
+            assert encrypted != "abc"
+            log.info("{}: Encrypted: {}", key.algorithm, encrypted)
+            def decrypted = strategy.decrypt(encrypted, key)
+            assert decrypted == "abc"
         }
     }
 
