762a8725fd1220136d37fa08173ff7c61ef8de6b
Mike Cantrell <mike@devnull.org>
2013-01-11 09:45:09 -0600

    renamed test

--- zuul-data/src/test/groovy/org/devnull/zuul/service/security/JasyptEncryptionStrategyIntegrationTest.groovy
+++ zuul-data/src/test/groovy/org/devnull/zuul/service/security/JasyptEncryptionStrategyIntegrationTest.groovy
@@ -9,9 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import javax.annotation.Resource
 
 @Slf4j
-class EncryptionStrategyIntegrationTest extends ZuulDataIntegrationTest {
+class JasyptEncryptionStrategyIntegrationTest extends ZuulDataIntegrationTest {
     @Autowired
-    EncryptionStrategy strategy
+    JasyptEncryptionStrategy strategy
 
     @Resource(name = 'keyMetaData')
     Map<String, KeyConfiguration> keyMetData
