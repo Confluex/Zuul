b8172330116ab983d0f0469ad3549489af0fc575
Mike Cantrell <mike@devnull.org>
2013-02-02 13:38:21 -0600

    started work on demo profile

---
+++ zuul-data/src/main/groovy/org/devnull/zuul/service/security/DemoAuthenticationProvider.groovy
@@ -0,0 +1,4 @@
+package org.devnull.zuul.service.security
+
+class DemoAuthenticationProvider {
+}
--- zuul-web/src/main/groovy/org/devnull/zuul/web/DemoLoginController.groovy
+++ zuul-web/src/main/groovy/org/devnull/zuul/web/DemoLoginController.groovy
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@Profile("security-ldap")
-class LdapLoginController {
+@Profile("security-demo")
+class DemoLoginController {
     @RequestMapping("/login")
     String login() {
         return "/login/form"
