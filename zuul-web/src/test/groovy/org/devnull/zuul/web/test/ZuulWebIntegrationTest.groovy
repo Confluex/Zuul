0c26cbfddd481a1f04509b42f02f0a4f94749f80
Mike Cantrell <mike@devnull.org>
2013-02-02 20:28:05 -0600

    fixed sql update to join new settings table to settings_group

--- zuul-web/src/test/groovy/org/devnull/zuul/web/test/ZuulWebIntegrationTest.groovy
+++ zuul-web/src/test/groovy/org/devnull/zuul/web/test/ZuulWebIntegrationTest.groovy
@@ -1,8 +1,8 @@
 package org.devnull.zuul.web.test
 
 import org.devnull.security.service.SecurityService
+import org.junit.After
 import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.ProviderManager
@@ -13,7 +13,6 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.transaction.annotation.Transactional
-import org.junit.After
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = [
@@ -22,7 +21,7 @@ import org.junit.After
 'classpath:security-context.xml'
 ])
 @Transactional('transactionManager')
-@ActiveProfiles("security-openid")
+@ActiveProfiles(["test", "security-openid"])
 abstract class ZuulWebIntegrationTest {
 
 
